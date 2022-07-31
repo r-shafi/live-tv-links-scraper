e4c202acebe24e808ca4a1e1dbd253bdd218a4e8
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-07-31 19:15:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const URL = 'http://tv.arrownetsylhet.com/';
+
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(URL);
+    const $ = cheerio.load(response.data);
+
+    const buttons = $('div.mt-4.flex.flex-wrap > button');
+
+    const links = [];
+
+    buttons.each((i, button) => {
+      const link = $(button).attr('value');
+      links.push({
+        link,
+        name: link.split('/')[3],
+      });
+    });
+
+    res.json(links);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
